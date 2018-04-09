0ae20dfe2ab24a76d6708629cb7151e01e5af411
Anfany <36292421+Anfany@users.noreply.github.com>
2018-04-09 16:04:13 +0800

    Update Softmax_Sklearn.py

--- Softmax Regression/Softmax_Sklearn.py
+++ Softmax Regression/Softmax_Sklearn.py
@@ -11,10 +11,10 @@ sklr = LogisticRegression(multi_class='multinomial', solver='sag', C=200, max_it
 
 #格式化输出混淆矩阵
 from prettytable import PrettyTable
-def confusion(realy, outy):
+def confusion(realy, outy, method='Sklearn'):
     mix = PrettyTable()
     type = sorted(list(set(realy.T[0])), reverse=True)
-    mix.field_names = [' '] + ['预测:%d类'%si for si in type]
+    mix.field_names = [method] + ['预测:%d类'%si for si in type]
     # 字典形式存储混淆矩阵数据
     cmdict = {}
     for jkj in type:
