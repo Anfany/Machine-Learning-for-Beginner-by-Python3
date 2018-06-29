672be226835eefe6ae231f20df210be30322bda7
Anfany <36292421+Anfany@users.noreply.github.com>
2018-06-29 17:50:18 +0800

    Update Linear_Regression_AnFany.py

--- Linear Regression/Linear_Regression_AnFany.py
+++ Linear Regression/Linear_Regression_AnFany.py
@@ -39,6 +39,7 @@ class LinearRegression:
             dJ_dw = 2 * np.dot(xdata.T, (y_predict - ydata)) / len(xdata)
 
 
+
             #更新系数w,b的值
             self.weights = self.weights - self.learn_rate * dJ_dw
 
@@ -51,25 +52,26 @@ class LinearRegression:
         return self.weights, cost_function
 
 
-    # 根据公式
+    #根据公式
     def Formula(self, xdata, ydata, func=Trans):
         xdata = func(xdata)
         self.weights = np.dot(np.dot(np.linalg.inv(np.dot(xdata.T, xdata)), xdata.T), ydata)
         y_predict = np.dot(xdata, self.weights)
-        cost = [np.sum((ydata - np.mean(ydata)) ** 2) / len(xdata)] # 开始是以y值得平均值作为预测值计算cost
-        cost += [np.sum((y_predict - ydata) ** 2) / len(xdata)] # 利用公式，一次计算便得到参数的值，不需要迭代
-        return self.weights, cost # 包括2个值
+        cost = [np.sum((ydata - np.mean(ydata)) ** 2) / len(xdata)] #开始是以y值得平均值作为预测值计算cost
+        cost += [np.sum((y_predict - ydata) ** 2) / len(xdata)]#利用公式，一次计算便得到参数的值，不需要迭代。
+        return self.weights, cost #包括2个值
 
-    # 预测
+    #预测
     def predict(self, xdata, func=Trans):
         return np.dot(func(xdata), self.weights)
 
-import matplotlib.pyplot as plt # 绘图
-from pylab import mpl 
-mpl.rcParams['font.sans-serif'] = ['SimHei']  # 中文字体
-mpl.rcParams['axes.unicode_minus'] = False    # 负号
+#绘图
+import matplotlib.pyplot as plt
+from pylab import mpl  # 作图显示中文
+mpl.rcParams['font.sans-serif'] = ['SimHei']  # 设置中文字体
+mpl.rcParams['axes.unicode_minus'] = False
 
-#绘图函数
+
 def figure(title, *datalist):
     for jj in datalist:
         plt.plot(jj[0], '-', label=jj[1], linewidth=2)
@@ -85,24 +87,29 @@ def getR(ydata_tr, ydata_pre):
     inexplicable = np.sum(((ydata_tr - ydata_pre) ** 2))
     return 1 - inexplicable / sum_error
 
+#  最终的程序
+if __name__ == "__main__":
+    regressor = LinearRegression()
+    # 开始训练
+    train_error = regressor.Gradient(lrdata[0], lrdata[1])
 
-regressor = LinearRegression()
-#开始训练
-train_error = regressor.Gradient(lrdata[0], lrdata[1])
+    # 用于预测数据的预测值
+    predict_result = regressor.predict(lrdata[2])
+    # 用于训练数据的预测值
+    train_pre_result = regressor.predict(lrdata[0])
 
-#用于预测数据的预测值
-predict_result = regressor.predict(lrdata[2])
-#用于训练数据的预测值
-train_pre_result = regressor.predict(lrdata[0])
+    # 绘制误差图
+    figure('误差图 最终的MSE = %.4f' % (train_error[1][-1]), [train_error[1], 'error'])
+
+    # 绘制预测值与真实值图
+    figure('预测值与真实值图 模型的' + r'$R^2=%.4f$' % (getR(lrdata[1], train_pre_result)), [predict_result, '预测值'],
+           [lrdata[3], '真实值'])
+    plt.show()
+
+    # 线性回归的参数
+    print('线性回归的系数为:\n w = %s, \nb= %s' % (train_error[0][:-1], train_error[0][-1]))
 
 
 
-#绘制误差图
-figure('误差图 最终的MSE = %.4f'%(train_error[1][-1]), [train_error[1], 'error'])
 
-#绘制预测值与真实值图
-figure('预测值与真实值图 模型的' + r'$R^2=%.4f$'%(getR(lrdata[1], train_pre_result)), [predict_result, '预测值'],[lrdata[3],'真实值'])
-plt.show()
 
-#线性回归的参数
-print('线性回归的系数为:\n w = %s, \nb= %s'%(train_error[0][:-1], train_error[0][-1]))
