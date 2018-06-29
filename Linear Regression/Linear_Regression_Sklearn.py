f222c938e38cc1bd25f07b366c430c43fcb279f9
Anfany <36292421+Anfany@users.noreply.github.com>
2018-06-29 17:51:54 +0800

    Update Linear_Regression_Sklearn.py

--- Linear Regression/Linear_Regression_Sklearn.py
+++ Linear Regression/Linear_Regression_Sklearn.py
@@ -10,6 +10,7 @@ from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
 
+
 #训练数据x
 x_train_data = lrdata[0]
 #预测数据x
@@ -19,18 +20,6 @@ x_predict_data = lrdata[2]
 #y数据shape转换(样本数，1)==> (样本数，)
 y_train_data = lrdata[1].T[0]
 y_predict_data = lrdata[3].T[0]
-
-
-reg = linear_model.LinearRegression()
-
-#开始训练
-reg.fit (x_train_data, y_train_data)
-
-#预测数据的预测值
-predict_result = reg.predict(x_predict_data)
-
-#训练数据的预测值
-train_pre_result = reg.predict(x_train_data)
 
 
 import matplotlib.pyplot as plt # 绘图
@@ -47,18 +36,39 @@ def figure(title, *datalist):
     plt.legend()
     plt.show()
 
+reg = linear_model.LinearRegression()
+#  最终的程序
+if __name__ == "__main__":
 
-train_error = [mean_squared_error(y_train_data, [np.mean(y_train_data)] * len(y_train_data)), mean_squared_error(y_train_data, train_pre_result)]
+    # 开始训练
+    reg.fit(x_train_data, y_train_data)
 
-#绘制误差图
-figure('误差图 最终的MSE = %.4f'%(train_error[-1]), [train_error, 'error'])
+    # 预测数据的预测值
+    predict_result = reg.predict(x_predict_data)
 
-#绘制预测值与真实值图
-figure('预测值与真实值图 模型的' + r'$R^2=%.4f$'%(r2_score(train_pre_result, y_train_data)), [predict_result, '预测值'],[y_predict_data,'真实值'])
-plt.show()
+    # 训练数据的预测值
+    train_pre_result = reg.predict(x_train_data)
 
-#线性回归的参数
-print('线性回归的系数为:\n w = %s \n b = %s'%(reg.coef_, reg.intercept_) )
+    train_error = [mean_squared_error(y_train_data, [np.mean(y_train_data)] * len(y_train_data)),
+                   mean_squared_error(y_train_data, train_pre_result)]
+
+    # 绘制误差图
+    figure('误差图 最终的MSE = %.4f' % (train_error[-1]), [train_error, 'error'])
+
+    # 绘制预测值与真实值图
+    figure('预测值与真实值图 模型的' + r'$R^2=%.4f$' % (r2_score(train_pre_result, y_train_data)), [predict_result, '预测值'],
+           [y_predict_data, '真实值'])
+    plt.show()
+
+    # 线性回归的参数
+    print('线性回归的系数为:\n w = %s \n b = %s' % (reg.coef_, reg.intercept_))
+
+
+
+
+
+
+
 
 
 
