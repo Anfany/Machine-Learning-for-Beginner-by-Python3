9f4fabd647db86214dc06faf0f28d9ae459deb5d
Anfany <36292421+Anfany@users.noreply.github.com>
2018-11-05 11:37:34 +0800

    Update Iris_Spyder.py

--- Softmax Regression/Iris_Spyder.py
+++ Softmax Regression/Iris_Spyder.py
@@ -9,35 +9,31 @@ html = urllib.request.urlopen("https://en.wikipedia.org/wiki/Iris_flower_data_se
 
 bsObj = bs(html.read(), "html5lib")
 
-nameList = bsObj.findAll('table',{'class' : "wikitable sortable mw-collapsible mw-collapsed"})
 
-datalist = str(nameList[0].get_text()).split('\n')
-
-
-data_set_name = datalist[1]
-
-#字段名称
-att_name = [fu.replace(' ','_') for fu in datalist[3:9]]
-
-for hh in att_name:
-    exec('%s = []'%hh)
-
-#开始获得数据
-for dat in range(len(datalist[9:-2])):
-    if dat % 8 > 1:
-        if dat % 8 != 7:
-            eval(att_name[dat % 8 - 2]).append(datalist[9:-2][dat])
-        else:
-
-            eval(att_name[dat % 8 - 2]).append(datalist[9:-2][dat].split('.')[1][1:])
-
+# 字段列表
+ziduan = []
+for hh in bsObj.find_all('table', class_='wikitable sortable mw-collapsible mw-collapsed'):
+    for ii in hh.find_all('th'):
+        fu = ii.get_text().split()
+        zi = ('_').join(fu)
+        exec('%s = []' % zi)
+        ziduan.append(zi)
+    fu = 0
+    for jj in hh.find_all('td'):
+        ty = jj.get_text().split()
+        try:
+            float(ty[0])
+            exec('%s.append(%.2f)' % (ziduan[fu % 6], float(ty[0])))
+        except ValueError:
+            exec('%s.append("%s")' % (ziduan[fu % 6], str(ty[-1])))
+        fu += 1
 
 
 #有序的字典形式,按添加的序列输出
 from collections import OrderedDict
 datadict=OrderedDict({})
 
-for keyname in att_name:
+for keyname in ziduan:
     datadict[keyname] = eval(keyname)
 
 
