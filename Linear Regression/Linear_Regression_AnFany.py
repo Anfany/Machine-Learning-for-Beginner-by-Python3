0f6c25f6a750db42a815b14ef4a1d3fd8e65bbc7
Anfany <36292421+Anfany@users.noreply.github.com>
2018-12-21 16:20:51 +0800

    Update Linear_Regression_AnFany.py

--- Linear Regression/Linear_Regression_AnFany.py
+++ Linear Regression/Linear_Regression_AnFany.py
@@ -7,21 +7,21 @@ import numpy as np
 
 #创建线性回归的类
 class LinearRegression:
+    #w和b合为一个参数，也就是x最后加上一列全为1的数据
 
     def __init__(self, learn_rate=0.2, iter_times=200000, error=1e-9):
         self.learn_rate = learn_rate
         self.iter_times = iter_times
         self.error = error
 
-    #w和b合为一个参数，也就是x最后加上一列全为1的数据。
     def Trans(self, xdata):
         one1 = np.ones(len(xdata))
         xta = np.append(xdata, one1.reshape(-1, 1), axis=1)
         return xta
 
     #梯度下降法
-    def Gradient(self, xdata, ydata, func=Trans):
-        xdata = func(xdata)
+    def Gradient(self, xdata, ydata):
+        xdata = self.Trans(xdata)
         #系数w,b的初始化
         self.weights = np.zeros((len(xdata[0]), 1))
         #存储成本函数的值
@@ -38,11 +38,8 @@ class LinearRegression:
             #计算梯度
             dJ_dw = 2 * np.dot(xdata.T, (y_predict - ydata)) / len(xdata)
 
-
-
             #更新系数w,b的值
             self.weights = self.weights - self.learn_rate * dJ_dw
-
 
             #提前结束循环的机制
             if len(cost_function) > 1:
@@ -53,17 +50,17 @@ class LinearRegression:
 
 
     #根据公式
-    def Formula(self, xdata, ydata, func=Trans):
-        xdata = func(xdata)
+    def Formula(self, xdata, ydata):
+        xdata = self.Trans(xdata)
         self.weights = np.dot(np.dot(np.linalg.inv(np.dot(xdata.T, xdata)), xdata.T), ydata)
         y_predict = np.dot(xdata, self.weights)
-        cost = [np.sum((ydata - np.mean(ydata)) ** 2) / len(xdata)] #开始是以y值得平均值作为预测值计算cost
-        cost += [np.sum((y_predict - ydata) ** 2) / len(xdata)]#利用公式，一次计算便得到参数的值，不需要迭代。
-        return self.weights, cost #包括2个值
+        cost = [np.sum((ydata - np.mean(ydata)) ** 2) / len(xdata)]  # 开始是以y值得平均值作为预测值计算cost
+        cost += [np.sum((y_predict - ydata) ** 2) / len(xdata)]  # 利用公式，一次计算便得到参数的值，不需要迭代。
+        return self.weights, cost  # 包括2个值
 
     #预测
-    def predict(self, xdata, func=Trans):
-        return np.dot(func(xdata), self.weights)
+    def predict(self, xdata):
+        return np.dot(self.Trans(xdata), self.weights)
 
 #绘图
 import matplotlib.pyplot as plt
